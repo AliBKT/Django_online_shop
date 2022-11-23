ac10e2d834ee9559dfb8409780b51824dc4dd150
Ali Baktash <alibaktash740@yahoo.com>
2022-11-23 15:59:59 -0500

    add bootstap to project

--- config/settings.py
+++ config/settings.py
@@ -36,9 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms'
+    
     # My installed app
     'accounts',
-    'pages'
+    'pages',
+
 ]
 
 MIDDLEWARE = [
