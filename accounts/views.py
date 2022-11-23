91c1d1e4d2180ca2c85f6c4f63f01783ef7a4505
Ali Baktash <alibaktash740@yahoo.com>
2022-11-23 15:39:00 -0500

    Implimentation signup and login views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
+from .froms import CustomUserCreationForm
 # Create your views here.
+
+
+class SingUpView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
+
+
--- pages/urls.py
+++ pages/urls.py
@@ -7,5 +7,4 @@ from . import views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name="home"),
     path('aboutus/', views.AboutUsView.as_view(), name="aboutus"),
-
 ]
