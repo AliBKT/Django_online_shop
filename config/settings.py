e65ea2750f1c8dbbea78249efbb868b3184e4d4b
Ali Baktash <alibaktash740@yahoo.com>
2022-12-23 10:09:21 -0500

    Add filter comment to inappropriate comments

--- config/settings.py
+++ config/settings.py
@@ -123,14 +123,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
+<<<<<<< HEAD
 # LANGUAGE_CODE = 'en-us'
+=======
+>>>>>>> main
 LANGUAGE_CODE = 'fa'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
---
+++ products/comment/inappropriateـwords/inappropriateـwords_en.py
@@ -0,0 +1,4 @@
+Inappropriateـwords_en = [
+    'dick', 'damn', 'piss off', 'dick head', 
+    'bastard', 'asshole', 'cock', 'pussy', 'ass', 
+    'bitch', 'fuck', 'shit', ]
